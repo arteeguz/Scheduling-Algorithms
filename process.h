ab71ed3ea42ba8230761774b50ab9d2b834462b1
Jawad Rada <103535961+jawadrada@users.noreply.github.com>
2023-12-02 19:27:32 -0500

    Update process.h

--- process.h
+++ process.h
@@ -10,12 +10,14 @@ public:
         this->burstTime = 0;
         this->arrivalTime = 0;
         this->waitingTime = 0;
+        this->totalSwitchTime = 0;
     }
     
     Process(int processId, int arrivalTime, int burstTime) {
         this->processId = processId;
         this->arrivalTime = arrivalTime;
         this->burstTime = burstTime;
+        this->remainingBurstTime = burstTime;
     }
     
     int getProcessId() {
@@ -62,8 +64,8 @@ public:
         return remainingBurstTime;
     }
 
-    void setRemainingBurstTime(int remaining) {
-        remainingBurstTime = remaining;
+    void setRemainingBurstTime(int remainingBurstTime) {
+        this->remainingBurstTime = remainingBurstTime;
     }
     
     int getArrivalTime() {
@@ -81,15 +83,24 @@ public:
     void setWaitingTime(int waitingTime) {
         this->waitingTime = waitingTime;
     }
+    
+    int getTotalSwitchTime(){
+        return totalSwitchTime;
+    }
+
+    void setTotalSwitchTime(int totalSwitchTime){
+        this->totalSwitchTime = totalSwitchTime;
+    }
 
 private:
     int processId; //id of the process
     int turnAroundTime; //Total time process spent in the system
     int arrivalTime; //Time the process enters the system
     int finishTime; //Time the process finishes executing
-    int burstTime; //Time required to execute ----
-    int remainingBurstTime = burstTime; //Don't change burstTime once its set, we need to reuse it for RR func
+    int burstTime; //Time required to execute process
+    int remainingBurstTime; //Don't change burstTime once its set, we need to reuse it for RR func
     int startTime; //Time the process beigns executing
     int waitingTime; //Time the process spent before beginning its execution
+    int totalSwitchTime;
 };
 #endif
